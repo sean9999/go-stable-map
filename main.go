d2dd29f45830243af47240692d20ead75e2cf7c3
Sean Macdonald <sean.r.macdonald@gmail.com>
2025-06-19 14:47:31 -0400

    slight signature change for better literacy

--- main.go
+++ main.go
@@ -20,8 +20,10 @@ func New[K comparable, V any]() *StableMap[K, V] {
 	}
 }
 
-func From[K comparable, V any](m map[K]V) StableMap[K, V] {
+func From[K comparable, V any](m map[K]V) *StableMap[K, V] {
 	sm := New[K, V]()
-	sm.Incorporate(m)
-	return *sm
+	if m != nil {
+		sm.Incorporate(m)
+	}
+	return sm
 }
