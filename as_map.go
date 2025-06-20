c8e73a8bc811fa372b62b48dea3412c0cfc82876
Sean Macdonald <sean.r.macdonald@gmail.com>
2025-06-20 18:38:58 -0400

    better handling of nil object

--- as_map.go
+++ as_map.go
@@ -1,8 +1,9 @@
 package stablemap
 
 func (sm *StableMap[K, V]) AsMap() map[K]V {
-	// sm.Lock()
-	// defer sm.Unlock()
+	if sm == nil {
+		return nil
+	}
 	m := make(map[K]V, sm.Length())
 	for k, v := range sm.Entries() {
 		m[k] = v
