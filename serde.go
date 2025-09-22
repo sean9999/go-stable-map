01e8661f08d7496dd637656d5bebd829a621d1dc
Sean M <Sean.macdonald@hardrockdigital.com>
2025-09-21 20:31:51 -0400

    marshalbinary must use lexical ordering

--- serde.go
+++ serde.go
@@ -1,6 +1,8 @@
 package stablemap
 
 import (
+	"slices"
+
 	"github.com/vmihailenco/msgpack/v5"
 )
 
@@ -10,11 +12,9 @@ type kvpair[K comparable, V any] struct {
 }
 
 func (sm *StableMap[K, V]) MarshalBinary() ([]byte, error) {
-
 	if sm == nil {
 		return nil, nil
 	}
-
 	pairs := make([]kvpair[K, V], 0, len(sm.index))
 	for k, v := range sm.Entries() {
 		kvp := kvpair[K, V]{k, v}
@@ -23,19 +23,20 @@ func (sm *StableMap[K, V]) MarshalBinary() ([]byte, error) {
 	return msgpack.Marshal(pairs)
 }
 
-func (sm *StableMap[K, V]) UnmarshalBinary(p []byte) error {
+func (lm *LexicalStableMap[K, V]) MarshalBinary() ([]byte, error) {
+	slices.Sort(lm.index)
+	return lm.StableMap.MarshalBinary()
+}
 
+func (sm *StableMap[K, V]) UnmarshalBinary(p []byte) error {
 	if p == nil {
 		return nil
 	}
-
 	var pairs []kvpair[K, V]
-
 	err := msgpack.Unmarshal(p, &pairs)
 	if err != nil {
 		return err
 	}
-
 	for _, pair := range pairs {
 		k := pair.K
 		v := pair.V
